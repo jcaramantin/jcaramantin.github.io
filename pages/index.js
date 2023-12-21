cfd305e0988b3c49a87b450847157e2047e325dc
jcaramantin <jcaramantin@sanms.com.pe>
2023-12-21 18:18:58 -0500

    First config for Next14 & React18

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import { Container, Box, Heading } from "@chakra-ui/react";
+
+const Page = () => {
+    return (
+        <Container>
+            <Box borderRadius="lg" bg="red" p={4} mb={6} align="center">
+                <div>Welcome to Jhoan Caramantin Website</div>
+            </Box>
+            <Box display={{md:'flex'}}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" size="lg" mb={2} variant="page-title">
+                        Jhoan Caramantin
+                    </Heading>
+                </Box>
+            </Box>
+        </Container>
+        
+    )
+}
+export default Page;
