38097d279dbb39b51aad3d65fedccb1757f3fc55
jcaramantin <jcaramantin@sanms.com.pe>
2024-01-13 12:52:01 -0500

    feat: add pfp

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import { Container, Box, Heading } from "@chakra-ui/react";
+import { Container, Box, Heading, Image, useColorModeValue } from "@chakra-ui/react";
 
 const Page = () => {
     return (
         <Container>
-            <Box borderRadius="lg" bg="red" p={4} mb={6} align="center">
+            <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={4} mb={6} align="center">
                 <div>Welcome to Jhoan Caramantin Website</div>
             </Box>
             <Box display={{md:'flex'}}>
@@ -12,6 +12,10 @@ const Page = () => {
                         Jhoan Caramantin
                     </Heading>
                     <p>( Desarrollador Full Stack / Ingeniero de Software / Artista ) </p>
+                </Box>
+                <Box flexShrink={0} mt={{base:4 ,md: 0}} ml={{md:6}} align="center">
+                    <Image  borderColor="whiteAlpha.800" borderWidth={2} borderStyle="solid" maxWidth="100px" 
+                            display="inline-block" borderRadius="full" src="/images/pfp.jpg" alt="ZHpfp"/>
                 </Box>
             </Box>
         </Container>
